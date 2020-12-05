34ac4c5641e73bf462680ffe3d918ff3bfc72571
wdavidl <davidinUCI@gmail.com>
2020-12-05 01:57:37 -0800

    Add animation and interaction

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// console.log("Your index.js file is loaded correctly!");
+
+$(".custombtn").hover( function() {
+    $(this).animate( {width:"200px"});
+
+                                   }
+,
+                       function() {
+     $(this).animate( {width:"150px"})                        
+                                  }
+
+                      );
